d2dbb20bbe9a5232c4a995b12d724ad0e7172d14
dangdung1996 <dung1996.it@gmail.com>
2020-12-24 10:11:19 +0700

    add file MylistTest

--- src/MyListTest.java
+++ src/MyListTest.java
@@ -1,5 +1,13 @@
 public class MyListTest {
     public static void main(String[] args) {
-
+        MyList<Integer> myList = new MyList<Integer>(10);
+        myList.add(0, 14);
+        myList.add(1,24);
+        myList.add(2,21);
+        myList.get(1);
+        System.out.println("Element : "+myList.get(1));
+        myList.remove(1);
+        System.out.println("Element : "+myList.get(1));
+        System.out.println("Element : "+myList.get(2));
     }
 }
